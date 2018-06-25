3e9750b273c92b8efcd1e22b7f42bd73d17da1f2
Jealvia <jealvia@espol.edu.ec>
2018-06-24 21:43:19 -0500

    Responsive completo

--- js/app.js
+++ js/app.js
@@ -28,11 +28,4 @@
     }
 })();
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+
